fed176fd29a97235e3ac235bfd44d67700abe663
aka-andi <andrea.motas@gmail.com>
2020-10-12 23:24:05 -0400

    made 'commands' folder for separate command files

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # csbot
 CS 321-004 Team 8 Semester Project
+====
 Brenda Henriquez, Andi Motas, Firaol Tolessa
 
 A Discord bot that prepares computer science students for their career by providing commands to receive a link to a random technical interview coding problem from LeetCode, a data structures trivia question from Hackernoon, and alerts about new career opportunities from LinkedIn. The bot allows the users in the Discord server to collaborate on coding questions, which builds a community in which peers can discuss and encourage each other for interview preparation. 
--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
 
-const prefix = config.prefix;
+const fs = require('fs');
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('bot is online!');
 });
 
-client.on('message', message =>{    // message from bot
+const config = require('./config.json');
+const prefix = config.prefix;
+client.on('message', message =>{ 
     if(!message.content.startsWith(prefix) || message.author.bot) return;
-
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if(command === 'ping'){
-        message.channel.send('pang');
-    } else if (command == 'youtube'){
+        client.commands.get('ping').execute(message, args);
+    } else if (command === 'youtube'){
         message.channel.send('https://www.youtube.com');
     }
-    // use elif for more commands...
+    
 });
 
 client.login(config.token);
