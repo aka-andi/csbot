9a24302e807baf1300615c58f3e5fbb2fd865f77
aka-andi <andrea.motas@gmail.com>
2020-10-22 21:21:56 -0400

    5-second interval alerts with ON status

--- commands/alerts.js
+++ commands/alerts.js
@@ -1,10 +1,19 @@
 var alertsOn = true;
+var interval;
 module.exports = {
     name: 'alerts',
     description: "alert command",
     execute(message, args) {
         alertsOn = (alertsOn == true) ? false : true;
-        var status = (alertsOn) ? 'ON' : 'OFF';
-        message.channel.send('Career opportunity alerts are now ' + status);
+        if (!alertsOn) {
+            clearInterval(interval);
+            message.channel.send('Career opportunity alerts are now OFF');
+        }
+        else {
+            message.channel.send('Career opportunity alerts are now ON');
+            interval = setInterval(function () {
+                message.channel.send("testing")
+            }, 5000);
+        }
     }
 }
