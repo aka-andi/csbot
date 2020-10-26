d99ae730a293207d97c80c6dd524b6808fccf1ef
aka-andi <andrea.motas@gmail.com>
2020-10-26 11:45:01 -0400

    used puppeteer to webscrape, added json results

--- opportunities.js
+++ opportunities.js
@@ -1,4 +1,39 @@
-const op1 = {
+const puppeteer = require("puppeteer");
+var fs = require("fs");
+(async () => {
+    try {
+        var browser = await puppeteer.launch({ headless: false });
+        var page = await browser.newPage();
+        await page.goto(`https://www.indeed.com/jobs?q=software%20engineer%20intern&l=Fairfax%2C%20VA&sort=date`, { waitUntil: 'networkidle2' });
+        var data = await page.evaluate(() => {
+            var headerList = document.querySelectorAll(`h2.title`);
+            var jobArr = [];
+            for (var i = 0; i < headerList.length; i++) {
+                var text = headerList[i].innerText;
+                var html = headerList[i].innerHTML;
+                jobArr[i] = {
+                    title: (text.indexOf("\n") > 0) ? text.substring(0, text.indexOf("\n")) : text,
+                    link: html.substring((html.indexOf("href=") + 6), (html.indexOf("onmousedown") - 2))
+                };
+            }
+            return jobArr;
+        });
+        await browser.close();
+        fs.writeFile("opportunities.json", JSON.stringify(data), function (err) {
+            if (err) throw err;
+            console.log("Saved!");
+        });
+        console.log("Browser Closed");
+    } catch (err) {
+        console.log(err);
+        await browser.close();
+        console.log("Browser Closed");
+    }
+})();
+
+
+
+/*const op1 = {
   name: 'Software Engineer Intern/Co-op (Herndon, VA)',
   link: 'https://www.indeed.com/viewjob?jk=74f53be4da1ae301&from=serp&vjs=3'
 }
@@ -19,4 +54,4 @@ message.channel.send({
       value: op2.link
     }],
   }
-});
+});*/
