4798fcf0218cc389820f394efafd6ba68435fc1b
aka-andi <andrea.motas@gmail.com>
2020-10-28 10:49:58 -0400

    fixed missing data, refactored opportunities json

--- commands/alerts.js
+++ commands/alerts.js
@@ -12,8 +12,31 @@ module.exports = {
         else {
             message.channel.send('Career opportunity alerts are now ON');
             interval = setInterval(function () {
-                message.channel.send("testing")
+                const op1 = {
+                    name: 'Software Engineer Intern/Co-op (Herndon, VA)',
+                    link: 'https://www.indeed.com/viewjob?jk=74f53be4da1ae301&from=serp&vjs=3'
+                }
+                const op2 = {
+                    name: 'Software Development Engineer Intern (Summer 2021)',
+                    link: 'https://www.indeed.com/viewjob?jk=109dd74e9e018229&from=serp&vjs=3'
+                }
+
+                message.channel.send({
+                    embed: {
+                        title: "New Career Opportunities!",
+                        description: "Last updated: " + new Date().toLocaleString(),
+                        fields: [{
+                            name: op1.name,
+                            value: op1.link
+                        },
+                        {
+                            name: op2.name,
+                            value: op2.link
+                        }],
+                    }
+                });
             }, 5000);
+
         }
     }
 }
--- opportunities.js
+++ opportunities.js
@@ -19,7 +19,7 @@ var fs = require("fs");
             return jobArr;
         });
         await browser.close();
-        fs.writeFile("opportunities.json", JSON.stringify(data), function (err) {
+        fs.writeFile("scraped.json", JSON.stringify(data), function (err) {
             if (err) throw err;
             console.log("Saved!");
         });
@@ -31,27 +31,3 @@ var fs = require("fs");
     }
 })();
 
-
-
-/*const op1 = {
-  name: 'Software Engineer Intern/Co-op (Herndon, VA)',
-  link: 'https://www.indeed.com/viewjob?jk=74f53be4da1ae301&from=serp&vjs=3'
-}
-const op2 = {
-  name: 'Software Development Engineer Intern (Summer 2021)',
-  link: 'https://www.indeed.com/viewjob?jk=109dd74e9e018229&from=serp&vjs=3'
-}
-message.channel.send({
-  embed: {
-    title: "New Career Opportunities!",
-    description: "Last updated: " + new Date().toLocaleString(),
-    fields: [{
-      name: op1.name,
-      value: op1.link
-    },
-    {
-      name: op2.name,
-      value: op2.link
-    }],
-  }
-});*/
