31dc34eee4366727494099e83ef2493945696617
aka-andi <andrea.motas@gmail.com>
2020-11-16 12:18:50 -0500

    restructure trivia functions so it can be exported

--- commands/trivia.js
+++ commands/trivia.js
@@ -1,41 +1,41 @@
-//npm install -save csv-parse
-
 const parse = require('csv-parse');
 const fs = require('fs');
-const csvData = []; 
+const csvData = [];
 const path = require('path');
-//const random = Math.floor(Math.random()* Math.floor(csvData.length));
-//{const r = random};
 
-//read trivia.csv file and create array of arrasy. Inner arrays represents one pair of question and answer
-//fs.createReadStream('/Users/brendahenriquez/Desktop/cs321/csbot/data/trivia.csv')
-fs.createReadStream(path.join('data', 'trivia.csv'))
-  .pipe(
+//read trivia.csv file and create array of array. Inner arrays represents one pair of question and answer
+function readFile() {
+  fs.createReadStream(path.join('data', 'trivia.csv'))
+    .pipe(
       parse({
         delimiter: ','
       })
-  )
-  .on('data', function(dataRow){
+    )
+    .on('data', function (dataRow) {
       csvData.push(dataRow);
-  })
-  .on('end', function(){
-    //console.log(csvData);
-  });
-  
-  
+    });
+}
 
-module.exports = {  
+//get random question
+function getQuestion() {
+  const random = Math.floor(Math.random() * Math.floor(csvData.length));
+  return trivia = {
+    question: csvData[random][0],
+    answer: csvData[random][1]
+  }
+}
+
+readFile();
+
+// Export functions to the command handler and test file
+module.exports = {
   name: 'trivia',
   description: "trivia command",
+  readFile: readFile,
+  getQuestion: getQuestion,
+  csvData: csvData,
   execute(message) {
-    //console.log(csvData[8][1]);
-    //get random question
-    const random = Math.floor(Math.random()* Math.floor(csvData.length));
-    const trivia = {
-      
-      question: csvData[random][0],
-      answer: csvData[random][1]
-    }
+    const trivia = getQuestion()
     module.exports = trivia;
     message.channel.send({
       embed: {
