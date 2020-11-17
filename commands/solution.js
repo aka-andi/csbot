b11df33fdda75147793c1bc2e97bcb5cff51eee8
Brenda Henriquez <brendahenriquez@Brendas-Air-2.fios-router.home>
2020-11-17 14:01:21 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/aka-andi/csbot into master""
    This reverts commit 56ffc9459984395a39cf29b621f8facba14e30a2.

--- commands/solution.js
+++ commands/solution.js
@@ -1,8 +1,24 @@
 module.exports = {
     name: 'solution',
     description: "solution command",
+<<<<<<< HEAD
     execute(message, args){
         message.channel.send('This command returns a link to ' +
         'the solution for the last sent LeetCode problem.');
+=======
+    execute(message) {
+        const problem = require('./problem.js')
+        if (problem.solution != undefined) {
+            message.channel.send({
+                embed: {
+                    title: "Coding Interview Practice!",
+                    description: '[View Solution for ' + problem.name + '](' + problem.solution + ')'
+                }
+            });
+        }
+        else {
+            message.reply('No coding problem has been given yet.');
+        }
+>>>>>>> 6bce4d04efa81a2508b883c700e7a49114e55b8c
     }
 }
