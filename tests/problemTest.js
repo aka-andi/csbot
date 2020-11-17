0178bf2fe2a6cd187de21b2257b437395579b760
Brenda Henriquez <brendahenriquez@Brendas-Air-2.fios-router.home>
2020-11-17 14:29:04 -0500

    installed pacakges

--- tests/problemTest.js
+++ tests/problemTest.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-// Chai test fil for the problems scraper script
-=======
 // Chai test file for the problems scraper script
->>>>>>> 1f1a2fced06c4804c21d9d950cf2d0212771a2ca
 var expect = require('chai').expect;
 var problems = require('../commands/problem');
