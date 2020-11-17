b4e742f8c4ee7768665e1ee691484f0800a9cbeb
Brenda Henriquez <brendahenriquez@Brendas-Air-2.fios-router.home>
2020-11-17 14:22:48 -0500

    trivia

--- commands/trivia.js
+++ commands/trivia.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 1f1a2fced06c4804c21d9d950cf2d0212771a2ca
 const parse = require('csv-parse');
 const fs = require('fs');
 const csvData = [];
@@ -9,7 +5,6 @@ const path = require('path');
 
 //read trivia.csv file and create array of array. Inner arrays represents one pair of question and answer
 function readFile() {
-<<<<<<< HEAD
   fs.createReadStream(path.join('data', 'trivia.csv'))
     .pipe(
       parse({
@@ -19,41 +14,21 @@ function readFile() {
     .on('data', function (dataRow) {
       csvData.push(dataRow);
     });
-=======
-    fs.createReadStream(path.join('data', 'trivia.csv'))
-        .pipe(
-            parse({
-                delimiter: ','
-            })
-        )
-        .on('data', function (dataRow) {
-            csvData.push(dataRow);
-        });
->>>>>>> 1f1a2fced06c4804c21d9d950cf2d0212771a2ca
 }
 
 //get random question
 function getQuestion() {
-<<<<<<< HEAD
   const random = Math.floor(Math.random() * Math.floor(csvData.length));
   return trivia = {
     question: csvData[random][0],
     answer: csvData[random][1]
   }
-=======
-    const random = Math.floor(Math.random() * Math.floor(csvData.length));
-    return trivia = {
-        question: csvData[random][0],
-        answer: csvData[random][1]
-    }
->>>>>>> 1f1a2fced06c4804c21d9d950cf2d0212771a2ca
 }
 
 readFile();
 
 // Export functions to the command handler and test file
 module.exports = {
-<<<<<<< HEAD
   name: 'trivia',
   description: "trivia command",
   readFile: readFile,
@@ -69,21 +44,4 @@ module.exports = {
       }
     });
   }
-=======
-    name: 'trivia',
-    description: "trivia command",
-    readFile: readFile,
-    getQuestion: getQuestion,
-    csvData: csvData,
-    execute(message) {
-        const trivia = getQuestion()
-        module.exports = trivia;
-        message.channel.send({
-            embed: {
-                title: "CS Trivia!",
-                description: trivia.question
-            }
-        });
-    }
->>>>>>> 1f1a2fced06c4804c21d9d950cf2d0212771a2ca
 }
